e5097b2c638d96befdbb63c02c15c12f48fa9175
blessing <blessingshongwe@gmail.com>
2025-01-18 13:15:37 +0200

    setup routes

--- storeFront/src/App.jsx
+++ storeFront/src/App.jsx
@@ -1,4 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./components/Layouts/MainLayout";
+import Home from "./pages/Home";
+import ProductsPage from "./pages/store/ProductsPage";
+import SingleProduct from "./pages/store/SingleProduct";
+import Cart from "./pages/store/Cart";
+import AuthLayout from "./components/Layouts/AuthLayout";
+import Login from "./pages/authentication/Login";
+import Register from "./pages/authentication/Register";
+import ResetPassword from "./pages/authentication/ResetPassword";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<ProductsPage />} />
+          <Route path='/single-product/:id' element={<SingleProduct />} />
+          <Route path='/cart' element={<Cart />} />
+        </Route>
+      </Routes>
+      <Route element={<AuthLayout />}>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='reset-password' element={<ResetPassword />} />
+      </Route>
+    </BrowserRouter>
+  );
 };
 export default App;
