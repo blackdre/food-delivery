619d6604c3eaa7fc9a32541dff67617c889c4f52
blessing <blessingshongwe@gmail.com>
2025-01-19 19:04:00 +0200

    Setup input form

--- dashboard/src/components/ui/Input.jsx
+++ dashboard/src/components/ui/Input.jsx
@@ -1,4 +1,13 @@
-const Input = () => {
-  return <div>Input</div>;
+import { Form } from "react-bootstrap";
+
+const Input = ({ type, placeholder, label }) => {
+  return (
+    <Form className='form-group'>
+      <Form.Label htmlFor={label} className='form-label'>
+        {label}
+      </Form.Label>
+      <Input type={type} className='form-control' placeholder={placeholder} />
+    </Form>
+  );
 };
 export default Input;
