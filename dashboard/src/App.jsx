f2c73da33e05d629f15e4b2d7ea2403cbe014984
blessing <blessingshongwe@gmail.com>
2025-01-19 09:44:17 +0200

    added dashboard routing roles

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,13 +1,43 @@
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./components/layouts/MainLayout";
+import Unauthorized from "./components/Unauthorized";
+import PrivateRoute from "./components/PrivateRoute";
+import AdminDashboard from "./pages/dashboard/AdminDashboard";
+import VendorDashboard from "./pages/dashboard/VendorDashboard";
+import DriverDashboard from "./pages/dashboard/DriverDashboard";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path='/' element={<Dashboard />} />
+        <Route
+          path='/admin'
+          element={
+            <PrivateRoute allowedRoles={["admin"]}>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path='/vendor'
+          element={
+            <PrivateRoute allowedRoles={["vendor"]}>
+              <VendorDashboard />
+            </PrivateRoute>
+          }
+        />
       </Route>
+      <Route
+        path='/driver'
+        element={
+          <PrivateRoute allowedRoles={["driver"]}>
+            <DriverDashboard />
+          </PrivateRoute>
+        }
+      />
+      <Route path='/unauthorized' element={<Unauthorized />} />
     </Routes>
   );
 };
